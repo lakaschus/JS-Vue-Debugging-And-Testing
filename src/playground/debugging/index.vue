2ef8a6e04d86b1b28ee2eab412a6ddb2e611c1bf
Phillip Lakaschus <Philliplakaschus@gmail.com>
2022-04-13 00:17:32 +0200

    Make dynamic component loading work

--- src/playground/debugging/index.vue
+++ src/playground/debugging/index.vue
@@ -5,46 +5,38 @@
       size="large"
       justify-content="space-evenly"
     >
-      <n-tab-pane :name="subdirectories[0]" :tab="subdirectories[0]" v-for="name in subdirectories">
-        <component :is="nextComponent"></component>
-      </n-tab-pane>
+<n-tab-pane :name="name" :tab="name" v-for="name in subdirectories">
+          <component :is="nextComponents(name)"></component>
+        </n-tab-pane>
     </n-tabs>
   </n-card>
 </template>
 <script>
 import { getAllDirectories } from "@/utils/os/getAllDirectories.js"
 import { NCard, NTabs, NTabPane } from "naive-ui"
-import {defineAsyncComponent} from "vue";
+import { defineAsyncComponent } from "vue"
 
 export default {
+  components: {
+    NCard,
+    NTabs,
+    NTabPane,
+  },
   data() {
     return {
       subdirectories: [],
-      moduleImports: []
+      moduleImports: [],
     }
   },
   mounted() {
     const modules = import.meta.glob("./**/index.vue")
-    for (const [key, value] of Object.entries(modules)) {
-        //import { test }
-        this.moduleImports.push(key)
-    }
-    console.log("🚀 ~ file: index.vue ~ line 26 ~ mounted ~ moduleNames", this.moduleImports[0])
-
     this.subdirectories = getAllDirectories(modules)
-    console.log("subdirs", this.subdirectories)
   },
-  computed: {
-    nextComponent() {
-    const modules = import.meta.glob("./**/index.vue")
-    var keyVal = ""
-    for (const [key, value] of Object.entries(modules)) {
-        //import { test }
-        keyVal = key
-    }
-    return defineAsyncComponent(() => import(keyVal))
-    }
-  }
+  methods: {
+    nextComponents(name) {
+      return defineAsyncComponent(() => import(`./${name}/index.vue`))
+    },
+  },
 }
 </script>
 <style lang=""></style>
